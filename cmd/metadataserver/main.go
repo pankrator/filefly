c26c00656e26000b2e0c3a71a3f2f9f1534e3e7c
Nikolay <pankrator@users.noreply.github.com>
2025-11-16 18:01:35 +0200

    Add web UI for metadata server

--- cmd/metadataserver/main.go
+++ cmd/metadataserver/main.go
@@ -12,6 +12,7 @@ func main() {
 	addr := flag.String("addr", ":8080", "address to listen on")
 	blockSize := flag.Int("block-size", 1024, "block size in bytes")
 	dataServers := flag.String("data-servers", ":8081", "comma separated list of data server addresses")
+	httpAddr := flag.String("http-addr", ":8090", "address for the optional web UI (empty to disable)")
 	flag.Parse()
 
 	var servers []string
@@ -23,6 +24,13 @@ func main() {
 	}
 
 	srv := metadataserver.New(*addr, *blockSize, servers)
+	if *httpAddr != "" {
+		go func() {
+			if err := srv.ListenHTTP(*httpAddr); err != nil {
+				log.Fatalf("metadata HTTP server failed: %v", err)
+			}
+		}()
+	}
 	if err := srv.Listen(); err != nil {
 		log.Fatalf("metadata server failed: %v", err)
 	}

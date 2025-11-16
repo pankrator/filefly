42a487e7909bcf9d1aab66927e759a46f797231f
Nikolay <pankrator@users.noreply.github.com>
2025-11-16 18:44:17 +0200

    Persist metadata snapshots

--- cmd/metadataserver/main.go
+++ cmd/metadataserver/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"strings"
+	"time"
 
 	"filefly/internal/metadataserver"
 )
@@ -12,6 +13,8 @@ func main() {
 	addr := flag.String("addr", ":8080", "address to listen on")
 	blockSize := flag.Int("block-size", 1024, "block size in bytes")
 	dataServers := flag.String("data-servers", ":8081", "comma separated list of data server addresses")
+	metadataFile := flag.String("metadata-file", "metadata.json", "path to persist metadata snapshots")
+	persistInterval := flag.Duration("persist-interval", 30*time.Second, "how often to persist metadata")
 	flag.Parse()
 
 	var servers []string
@@ -22,7 +25,7 @@ func main() {
 		}
 	}
 
-	srv := metadataserver.New(*addr, *blockSize, servers)
+	srv := metadataserver.New(*addr, *blockSize, servers, *metadataFile, *persistInterval)
 	if err := srv.Listen(); err != nil {
 		log.Fatalf("metadata server failed: %v", err)
 	}
